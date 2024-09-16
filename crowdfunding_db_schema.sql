3e16f074bfb1c371bd73dd32ac34945120a711d9
BrendanD2 <brendandoucette2021@gmail.com>
2024-09-15 23:24:51 -0400

    adding final python changes

---
+++ crowdfunding_db_schema.sql
@@ -0,0 +1,66 @@
+DROP TABLE IF EXISTS campaign;
+DROP TABLE IF EXISTS category;
+DROP TABLE IF EXISTS contacts;
+DROP TABLE IF EXISTS subcategory;
+
+CREATE TABLE "campaign" (
+    "cf_id" int   NOT NULL,
+    "contact_id" int   NOT NULL,
+    "company_name" varchar(100)   NOT NULL,
+    "description" text   NOT NULL,
+    "goal" float   NOT NULL,
+    "pledged" float   NOT NULL,
+    "outcome" varchar(25)   NOT NULL,
+    "backers_count" int   NOT NULL,
+    "country" varchar(25)   NOT NULL,
+    "launched_date" date   NOT NULL,
+    "end_date" date   NOT NULL,
+    "category_id" varchar(25)   NOT NULL,
+    "subcategory_id" varchar(25)   NOT NULL,
+    CONSTRAINT "pk_campaign" PRIMARY KEY (
+        "cf_id"
+     )
+);
+
+CREATE TABLE "category" (
+    "category_id" varchar(25)   NOT NULL,
+    "category" varchar(25)   NOT NULL,
+    CONSTRAINT "pk_category" PRIMARY KEY (
+        "category_id"
+     )
+);
+
+CREATE TABLE "subcategory" (
+    "subcategory_id" varchar(25)   NOT NULL,
+    "subcategory" varchar(25)   NOT NULL,
+    CONSTRAINT "pk_subcategory" PRIMARY KEY (
+        "subcategory_id"
+     )
+);
+
+CREATE TABLE "contacts" (
+    "contact_id" int   NOT NULL,
+    "first_name" varchar(25)   NOT NULL,
+    "last_name" varchar(25)   NOT NULL,
+    "email" varchar(50)   NOT NULL,
+    CONSTRAINT "pk_contacts" PRIMARY KEY (
+        "contact_id"
+     )
+);
+
+ALTER TABLE "campaign" ADD CONSTRAINT "fk_campaign_contact_id" FOREIGN KEY("contact_id")
+REFERENCES "contacts" ("contact_id");
+
+ALTER TABLE "campaign" ADD CONSTRAINT "fk_campaign_category_id" FOREIGN KEY("category_id")
+REFERENCES "category" ("category_id");
+
+ALTER TABLE "campaign" ADD CONSTRAINT "fk_campaign_subcategory_id" FOREIGN KEY("subcategory_id")
+REFERENCES "subcategory" ("subcategory_id");
+
+SELECT * FROM campaign;
+
+SELECT * FROM category;
+
+SELECT * FROM contacts;
+
+SELECT * FROM subcategory;
